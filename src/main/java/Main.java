c0d43f9b1c125e30469426c243e3987dd4053737
M4rtR0d <62573083+M4rtR0d@users.noreply.github.com>
2022-09-08 10:56:52 -0500

    primer commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+
+
